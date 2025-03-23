0ceb721a00af7bda3534ef8d0c4ecf10039d430e
Jacob <jacobmpav@gmail.com>
2025-03-23 15:55:46 -0400

    Triggering CI/CD pipeline

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-# What an awesome and cool app!
+# What an awesome and super cool app!
 
 @app.route('/api/message', methods=['GET'])
 def get_message():
